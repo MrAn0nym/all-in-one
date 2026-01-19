4cedde29360411303dbb0b165ec5cee608c58c4a
Pablo Zmdl <pablo@nextcloud.com>
2026-01-19 12:21:23 +0100

    Make `collabora_additional_options` an attribute
    Signed-off-by: Pablo Zmdl <pablo@nextcloud.com>

--- php/src/Docker/DockerActionManager.php
+++ php/src/Docker/DockerActionManager.php
@@ -422,8 +422,8 @@ readonly class DockerActionManager {
             }
 
             // Additional Collabora options
-            if ($this->configurationManager->GetAdditionalCollaboraOptions() !== '') {
-                $requestBody['Cmd'] = [$this->configurationManager->GetAdditionalCollaboraOptions()];
+            if ($this->configurationManager->collabora_additional_options !== '') {
+                $requestBody['Cmd'] = [$this->configurationManager->collabora_additional_options];
             }
         }
 
